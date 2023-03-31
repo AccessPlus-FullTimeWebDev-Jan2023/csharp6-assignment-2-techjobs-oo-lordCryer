8ac7d8620db4b92d4453e3be13535672d58e8681
Dion <lordcryer@yahoo.com>
2023-03-31 17:33:46 -0500

    Task 3.5

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -1,17 +1,60 @@
 ﻿
 namespace TechJobs.Tests
 {
-	[TestClass]
-	public class JobTests
-	{
+    [TestClass]
+    public class JobTests
+    {
         //Testing Objects
+
+        Job job1 = new Job();
+
+        Job job2 = new Job();
+
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+
         //initalize your testing objects here
 
-        [TestMethod]
-        public void TestMethod()
+ 
+    
+    
+        
+            //TODO: add emptyTest so we can configure our runtime environment
+            [TestMethod]
+        public void EmptyTest()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            Assert.AreEqual(10,10,.001);
         }
+        [TestMethod]
+
+        public void TestSettingJobId ()
+        {
+            int actual = job2.Id - job1.Id;
+            Assert.AreEqual(1, actual);
+
+        }
+        [TestMethod]    
+
+        public void TestJobConstructorSetsAllFields ()
+        {
+            string actual = job3.Name;
+            Assert.AreEqual("Product tester", actual);
+            string actual1 = job3.EmployerName.Value;
+            Assert.AreEqual("ACME", actual1);
+            string actual2 = job3.EmployerLocation.Value;
+            Assert.AreEqual("Desert", actual2);
+            string actual3 = job3.JobType.Value;
+            Assert.AreEqual("Quality control", actual3);
+            string actual4 = job3.JobCoreCompetency.value;
+            Assert.AreEqual("Persistence", actual4);
+
+
+        }
+        // ... other TODOs omitted here
+
+
     }
 }
 
